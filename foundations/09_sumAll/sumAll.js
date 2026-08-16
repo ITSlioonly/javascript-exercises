5a707ba0bfd0137ee0650db46db08ce911cc57fe
ITSlioonly <levshchygrov@gmail.com>
2026-08-16 11:10:00 +0200

    Finished ex09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0
+    if(typeof(a) !== "number" || typeof(b) !== "number" || a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(a)) {
+        return "ERROR"
+    }
+    if(a < b) {
+        for(; b >= a; b--) {
+            sum += b;
+        }
+    } else if(a > b) {
+        for(; a >= b; a--) {
+            sum += a;
+        }
+    } else {
+        return a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
