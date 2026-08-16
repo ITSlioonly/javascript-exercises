461a943f824d7b62ceb64574c9f5db66b9c648f2
ITSlioonly <levshchygrov@gmail.com>
2026-08-16 10:55:58 +0200

    Finished ex07, ex08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, a, b, c, d) {
+    for(let i = 0; i < arr.length;) {
+        if(arr[i] === a || arr[i] === b || arr[i] === c || arr[i] === d) {
+            arr.splice(i, 1)
+        } else {
+            i++
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
