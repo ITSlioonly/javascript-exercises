7954ac0747bf7f368d28f26384789f4b6a6bfdbf
ITSlioonly <levshchygrov@gmail.com>
2026-08-18 11:12:51 +0200

    Finished it

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arr = str
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, "")
+        .split("");
 
+    let rev = arr.toReversed();
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== rev[i]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS"
+    let lastNum = 1;
+    let newNum = 0;
+    let curr = 0;
+    for(; num > 0; num--) {
+        curr = newNum + lastNum;
+        lastNum = newNum;
+        newNum = curr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
